8a2e082ebb4d03ca2ffc95b4d031bcd2909e64d4
BalandinaM <marina.balandina.89@mail.ru>
2026-01-26 22:36:52 +0500

    init

---
+++ src/TodolistItem.tsx
@@ -0,0 +1,42 @@
+import { Task } from "./App";
+import { Button } from "./Button";
+
+type Props = {
+  title: string;
+  tasks: Task[];
+  date?: string;
+};
+
+export const TodolistItem = ({ title, tasks, date }: Props) => {
+  return (
+    <div>
+      <div>
+        <h3>{title}</h3>
+        <div>
+          <input />
+          <button>+</button>
+        </div>
+        {tasks.length === 0 ? (
+          <p>Тасок нет</p>
+        ) : (
+          <ul>
+            {tasks.map((task) => {
+              return (
+                <li key={task.id}>
+                  <input type="checkbox" checked={task.isDone} />
+                  <span>{task.title}</span>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        <div>{date}</div>
+        <div>
+          <Button title="All" />
+          <Button title="Active" />
+          <Button title="Completed" />
+        </div>
+      </div>
+    </div>
+  );
+};
