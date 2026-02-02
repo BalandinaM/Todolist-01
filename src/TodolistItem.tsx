9b54ba335e0d3dcf369ff5fa06ab944196ae15c4
BalandinaM <marina.balandina.89@mail.ru>
2026-02-02 11:20:13 +0500

    2 занятие

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -5,9 +5,10 @@ type Props = {
   title: string;
   tasks: Task[];
   date?: string;
+  deleteTask: (taskId: number) => void
 };
 
-export const TodolistItem = ({ title, tasks, date }: Props) => {
+export const TodolistItem = ({ title, tasks, date, deleteTask, changeFilter }: Props) => {
   return (
     <div>
       <div>
@@ -25,6 +26,7 @@ export const TodolistItem = ({ title, tasks, date }: Props) => {
                 <li key={task.id}>
                   <input type="checkbox" checked={task.isDone} />
                   <span>{task.title}</span>
+                  <Button onClick={() => deleteTask(task.id)} title={"X"} />
                 </li>
               );
             })}
@@ -32,9 +34,9 @@ export const TodolistItem = ({ title, tasks, date }: Props) => {
         )}
         <div>{date}</div>
         <div>
-          <Button title="All" />
-          <Button title="Active" />
-          <Button title="Completed" />
+          <Button title="All" onClick={() => changeFilter('all')}/>
+          <Button title="Active" onClick={() => changeFilter('active')}/>
+          <Button title="Completed" onClick={() => changeFilter('completed')}/>
         </div>
       </div>
     </div>
